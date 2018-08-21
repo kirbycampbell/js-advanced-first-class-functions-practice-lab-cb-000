002f41a9a54994d9ba272c0cb89d36bd7ea933b5
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-21 22:23:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const logDriversByHometown = function (drivers, hometown) {
 };
 
 const driversByRevenue = function (drivers) {
-    return drivers.sort(function(driverA, driverB){
+    return drivers.slice().sort(function(driverA, driverB){
       return driverA.revenue - driverB.revenue;
     });
 }
